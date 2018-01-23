b2d481de8e2a34789009f8ae503652bdc08b6242
李文毅 <liwenyi@jiagouyun.com>
2018-01-23 17:41:26 +0800

    test

---
+++ hello.py
@@ -0,0 +1,28 @@
+# coding:utf-8
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Index Page'
+
+
+@app.route('/hello')
+def hello():
+    return '你好'
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return 'User %s' % username
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return 'Post %d' % post_id
+
+
+if __name__ == '__main__':
+    #app.debug = True
+    app.run(host='0.0.0.0',debug=True)
+
